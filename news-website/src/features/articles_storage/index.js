96518cb481470fa6e654fd6be60374a5c6015289
Jason <ivandomogatskij@gmail.com>
2025-10-09 17:57:48 +0300

    pork article

--- news-website/src/features/articles_storage/index.js
+++ news-website/src/features/articles_storage/index.js
@@ -3,11 +3,13 @@ import { aiWarningArticle } from "./articles/aiWarning";
 import { onlineCompilersArticle } from "./articles/onlineComplillers";
 import { gossvat2Article } from "./articles/gossvat2";
 import pancakesArticle from "./articles/pancakes";
+import { porkConspiracyArticle } from "./articles/porkConspiracyArticle";
 
 export const articlesStorage = {
+    "pork-conspiracy-ancient-teachings": porkConspiracyArticle,
+    "great-pancake-conspiracy": pancakesArticle,
     "messenger-gossvat": gossvatArticle,
     "ai-warning": aiWarningArticle,
     "online-compilers-danger": onlineCompilersArticle,
     "gossvat-2-mamenkin-messenger": gossvat2Article,
-    "great-pancake-conspiracy": pancakesArticle
 };
