e963a6076153194c9cab2322183c51b7d1c1c946
Jason1258t <ivandomogatskij@gmail.com>
2025-11-16 12:13:09 +0300

    icons

--- src/features/horoscope/model/zodiac-icons.js
+++ src/features/horoscope/model/zodiac-icons.js
@@ -1,14 +1,14 @@
 export const zodiacIcons = {
-    "Aries": "",
-    "Taurus": "",
-    "Gemini": "",
-    "Cancer": "",
-    "Leo": "",
-    "Virgo": "",
-    "Libra": "",
-    "Scorpio": "",
-    "Sagittarius": "",
-    "Capricorn": "",
-    "Aquarius": "",
-    "Pisces": ""
+    "Aries": "https://i.pinimg.com/736x/c3/eb/9f/c3eb9f2af3a39b40a24831c3173fc8d3.jpg",
+    "Taurus": "https://i.pinimg.com/736x/4a/5b/67/4a5b67561bcfa50025f4ed36eb0133b0.jpg",
+    "Gemini": "https://i.pinimg.com/736x/4a/10/29/4a1029cbda51db40de5ad6bb1ea06883.jpg",
+    "Cancer": "https://i.pinimg.com/736x/4b/f8/bd/4bf8bdc84177f2c839dfb7ddaf725e93.jpg",
+    "Leo": "https://i.pinimg.com/736x/4f/f2/3a/4ff23af5dbeef5e2f04f00f27e0cf65e.jpg",
+    "Virgo": "https://i.pinimg.com/736x/99/67/be/9967bec5f7a711719f4c79f867ca0b26.jpg",
+    "Libra": "https://i.pinimg.com/736x/18/84/fb/1884fba69157ef1897780d49232cf314.jpg",
+    "Scorpio": "https://i.pinimg.com/736x/9a/db/1e/9adb1ea7ae69c18a5a276a17d6143a3c.jpg",
+    "Sagittarius": "https://i.pinimg.com/736x/49/e9/5a/49e95a70dc917c6a7bc2471e352d790f.jpg",
+    "Capricorn": "https://i.pinimg.com/736x/e8/4c/8a/e84c8a29385be88ab58615b20b417106.jpg",
+    "Aquarius": "https://i.pinimg.com/736x/be/35/6a/be356ae6ac21169a4450d01abb7868a5.jpg",
+    "Pisces": "https://i.pinimg.com/736x/3d/3a/de/3d3ade0c5886a5f99811dfb1d05c3d0b.jpg"
 };
